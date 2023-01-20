a047cfc5c0d4d9510107a457b6180f54d732336c
Arcangelo Calamusa <117526559+ArCalamusa@users.noreply.github.com>
2023-01-20 21:09:43 +0100

    Add files via upload

---
+++ W1-P1.js
@@ -0,0 +1,5 @@
+//1. Cos'è un algoritmo? Prova a spiegarlo con parole tue, come se lo dovessi spiegare a qualcuno che non ne capisce niente di informatica: dato un obbiettivo è l'insieme delle operazioni atte a raggiungerlo.
+
+//2. Cos'è una variabile? Prova a spiegarlo con parole tue, come se lo dovessi spiegare a qualcuno che non ne capisce niente di informatica: è un contenitore di dati che possiede un etichetta/nome.
+
+// 3. undefined e null sono la stessa cosa (falso): null indica che alla variabile non è stato assegnato un valore di proposito; undefined: indica, invece, una variabile non inizializzata, cioè a cui non è stato ancora assegnato un valore
